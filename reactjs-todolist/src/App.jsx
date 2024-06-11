ff74dd17d0c00bc0aa4a33ef3b687e017b57933a
javanpang <javanpang@hotmail.com>
2024-06-11 02:26:48 +0100

    Add functionality to save todo list

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
@@ -7,18 +7,25 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState("");
 
-  // Define the handleAddTodos function to update the todos state by adding a new todo.
+  // Define persistData function to store todos in local storage
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+  }
+
+  // Define the handleAddTodos function to update the todos state by adding a new todo and update local storage.
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
     setTodos(newTodoList);
   }
 
-  // Define the handleDeleteTodo function to remove a todo from the todos list based on the index.
+  // Define the handleDeleteTodo function to remove a todo from the todos list based on the index and update local storage.
   function handleDeleteTodo(index) {
     // Create a new array newTodoList by filtering out the todo item at the specified index.
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index;
     });
+    persistData(newTodoList);
     // Update the todos state with the new filtered array.
     setTodos(newTodoList);
   }
@@ -30,6 +37,22 @@ function App() {
     setTodoValue(valueToBeEdited);
     handleDeleteTodo(index);
   }
+
+  // Retrieves todos from local storage and sets todos state when component mounts.
+  useEffect(() => {
+    // Check if localStorage is available
+    if (!localStorage) {
+      return;
+    }
+    // Retrieve todos from local storage "todos"
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+    // Parse the retrieved todos and update the todos state with the parsed value.
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
 
   // Render TodoInput component to add todos and TodoList component to display todos
   return (
